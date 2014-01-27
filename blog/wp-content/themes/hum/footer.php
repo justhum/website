4fcb1ebad89a12823147a8d15be6ed0def78bf55
Aaron Shekey <aaron@aaronshekey.com>
2014-01-26 21:41:18 -0600

    Add Real Download Buttons

--- blog/wp-content/themes/hum/footer.php
+++ blog/wp-content/themes/hum/footer.php
@@ -1,17 +1,16 @@
 
 		</div>
-
+		
 		<div class="section footer">
             <h1 class="logo"><a href="/">Hum</a></h1>
             <h2>Your Songwriting, Organized</h2>
             
             <ul>
-                <li><a href="/about/" title="About">About</a></li>
-                <li><a href="/blog/" class="current" title="Blog">Blog</a></li>
-                <li><a href="/support/" title="Support">Support</a></li>
+            	<li><a href="/about/" class="current">About</a></li>
+                <li><a href="/blog/">Blog</a></li>
+                <li><a href="/support/">Support</a></li>
+                <li><a href="https://itunes.apple.com/us/app/hum/id796154884?ls=1&mt=8" class="appstore_btn" target="_blank">Download on <br>the App Store</a></li>
             </ul>
-            
-            <!-- <li><a href="https://itunes.apple.com/us/app/hum/id796154884?ls=1&mt=8" title="Download on the App Store" class="appstore_btn">Download on <br>the App Store</a></li> -->
             
             <form action="http://justhum.us5.list-manage.com/subscribe/post?u=66c48ed94c753b5fb093c3807&amp;id=c7fec5ee88" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form">
             	<input type="email" value="" name="EMAIL" class="email" id="mc-email" placeholder="Email Address" required spellcheck="false">
@@ -19,24 +18,9 @@
             	<label for="mc-email"></label>
             </form>
         </div>
-
-		<?php wp_footer(); ?>
-		
+				
 		<script src="//code.jquery.com/jquery-2.0.3.min.js"></script>
-        <script src="/_/js/jquery.retina.min.js"></script>
-        <script src="/_/js/jquery.ajaxchimp.js"></script>
-  
-        <script type="text/javascript">        
-            $(window).load(function(){      
-                $("img").retina({
-                    suffix: "@2x",
-                    checkIfImageExists: true
-                });
-                
-                $('#mc-embedded-subscribe-form').ajaxChimp();
-            });
-        </script>
-        
+        <script src="/_/js/production/secondary.min.js"></script>
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
