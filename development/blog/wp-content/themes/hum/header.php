477be7efd2e484c6a25c50e37093ed4e23c6a7a3
Aaron Shekey <aaronshekey@github.com>
2016-03-28 11:26:23 -0500

    Blog title

--- development/blog/wp-content/themes/hum/header.php
+++ development/blog/wp-content/themes/hum/header.php
@@ -28,7 +28,7 @@ MMMMMMO  .
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-        <title><?php wp_title(' &ndash; ', true, 'right'); ?></title>
+        <title><?php wp_title( ' &ndash; ', true, 'right' ); ?><?php bloginfo('name'); ?></title>
         <meta name="description" content="Hum. Songwriting for iPhone.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
