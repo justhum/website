d6ae1ecceb3eec392be5c78c0cdcb70a09ddb795
Aaron Shekey <aaron@aaronshekey.com>
2017-03-04 14:57:24 -0600

    Tuner and punctuation

--- development/support/index.php
+++ development/support/index.php
@@ -50,56 +50,53 @@
 
       <div class="entry-content">
         <h3>How do I add a Hum? <a class="video-btn" href="http://www.youtube.com/watch?v=KLVBF_ekwfg">Play</a></h3>
-        <p>Start by tapping the plus button in the top right of your library. You&rsquo;ll find a page for lyrics, info, and notes. When you tap into the lyrics field you&rsquo;ll notice that the first line becomes your title. In the info page will help you categorize your Hum by key, tuning, capo, and mood. The notes pane lets you add all sorts of journal-like context to your Hum. The audio recorder sits at the bottom of everything.</p>
+        <p>Start by tapping the plus button in the top right of your library. You’ll find a page for lyrics, info, and notes. When you tap into the lyrics field you’ll notice that the first line becomes your title. In the info page will help you categorize your Hum by key, tuning, capo, and mood. The notes pane lets you add all sorts of journal-like context to your Hum. The audio recorder sits at the bottom of everything.</p>
 
         <h3>How do I sort my library? <a class="video-btn" href="http://www.youtube.com/watch?v=M073ppBuPaM">Play</a></h3>
-        <p>Tap the sort button at the top left of your library. Here you can do all sorts of sorting by newest / oldest, lyrics or audio only, key, tuning, capo, mood, or dynamics, or any combination of any of those. It&rsquo;s really quite powerful once you've got a bunch of ideas. For example, Hum can show you all your sad ideas in the key of G that only have audio.</p>
+        <p>Tap the sort button at the top left of your library. Here you can do all sorts of sorting by newest / oldest, lyrics or audio only, key, tuning, capo, mood, or dynamics, or any combination of any of those. It’s really quite powerful once you've got a bunch of ideas. For example, Hum can show you all your sad ideas in the key of G that only have audio.</p>
 
-        <h3>How do I change Hum&rsquo;s settings? <a class="video-btn" href="http://www.youtube.com/watch?v=ssFFv888Wno">Play</a></h3>
-        <p>We&rsquo;ve added a few options in Hum. If you tap the gear at the lower right of your library you&rsquo;ll find preferences for Key Notation, Recording Countdown, and Usage Statistics. You&rsquo;ll also find links to get help and leave us feedback.</p>
+        <h3>How do I change Hum’s settings? <a class="video-btn" href="http://www.youtube.com/watch?v=ssFFv888Wno">Play</a></h3>
+        <p>We’ve added a few options in Hum. If you tap the gear at the lower right of your library you’ll find preferences for Key Notation, Recording Countdown, and Usage Statistics. You’ll also find links to get help and leave us feedback.</p>
 
         <h3>How do I delete a Hum? <a class="video-btn" href="http://www.youtube.com/watch?v=E3VIugsJu10">Play</a></h3>
         <p>In your library, swipe from right to left. Tap the delete button that appears.</p>
 
         <h3>So, wait. Hum lets you sing a melody into your phone and it converts it into music? Is it MIDI or something? Or is it more like Shazam?</h3>
-        <p>Nope. Hum combines note-taking and audio recording into a single app. Capture new ideas, filter through older ones and share with your bandmates or friends. So, while Hum will <em>not</em> transpose any sound into written music, it will make you a more organized and efficient songwriter.</p>
+        <p>Nope. Hum doesn’t do any of those things. Hum combines note-taking and audio recording into a single app. Capture new ideas, filter through older ones and share with your bandmates or friends. So, while Hum will <em>not</em> transcribe a sound you hum into written music, it will make you a more organized and efficient songwriter. It only records what you sing into it to capture your melodic ideas.</p>
 
         <h3>Will Hum be released for iPad, Android, Windows Phone, web, etc&hellip;?</h3>
-        <p>Not yet. We want to first make sure Hum is amazing on the iPhone. From there we&rsquo;ll explore branching out to different platforms.</p>
+        <p>Not yet. We want to first make sure Hum is amazing on the iPhone. From there we’ll explore branching out to different platforms.</p>
 
         <h3>Is there a trial version?</h3>
-        <p>Not unless Apple changes their store policies. Currently, app developers can&rsquo;t offer trial versions of their apps. If we were able, we&rsquo;d love to.</p>
-
-        <h3>Why do I have to pay for your tuner?</h3>
-        <p>All we&rsquo;ve ever wanted to do was make the best tool for songwriters. We can&rsquo;t do this with one-time App Store revenue alone. For non-essential features, we&rsquo;re experimenting with ways to add revenue so we can add other features that benefit <em>every</em> user of Hum.</p>
+        <p>Not unless Apple changes their store policies. Currently, app developers can’t offer trial versions of their apps. If we were able, we’d love to.</p>
 
         <h3>What audio format do you record in?</h3>
         <p>All our recordings are .m4a AAC audio files at 44.1kHz and encoded at 128kbps. They should work just fine with your DAW.</p>
 
         <h3>What is Dropbox syncing?</h3>
-        <p>Dropbox syncing allows us to keep a copy of your library in the cloud. This way, if your phone&rsquo;s stolen, or lost, or just quits working, you can load up a fresh copy of Hum on your new device, sync to Dropbox, and the whole library will sync down to your new phone. Or if you&rsquo;re an eccentric billionaire with more than one iPhone, you can sync between those.</p>
-        <p>This doesn&rsquo;t mean you&rsquo;ll be able to edit your Hums on the computer. It&rsquo;s not like that. We&rsquo;re using Dropbox as an alternative to iCloud syncing. By choosing Dropbox, we&rsquo;ve got a syncing service that&rsquo;ll work across all the possible platforms Hum could be published on.</p>
-        <p>This also doesn't mean you&rsquo;ll be able to invite people to your Dropbox folder so they can see all your changes and edits. Even though that might work, Hum&rsquo;s syncing wasn&rsquo;t designed this way.</p>
-        <p>Hum's folder in Dropbox meant to be read-only. If you edit any of these files by hand, or move them, you could ruin Hum&rsquo;s ability to sync to it, risking your whole library. If you still want to use your audio, you&rsquo;ll want to copy the files out of Dropbox and edit them elsewhere.</p>
+        <p>Dropbox syncing allows us to keep a copy of your library in the cloud. This way, if your phone’s stolen, or lost, or just quits working, you can load up a fresh copy of Hum on your new device, sync to Dropbox, and the whole library will sync down to your new phone. Or if you’re an eccentric billionaire with more than one iPhone, you can sync between those.</p>
+        <p>This doesn’t mean you’ll be able to edit your Hums on the computer. It’s not like that. We’re using Dropbox as an alternative to iCloud syncing. By choosing Dropbox, we’ve got a syncing service that’ll work across all the possible platforms Hum could be published on.</p>
+        <p>This also doesn't mean you’ll be able to invite people to your Dropbox folder so they can see all your changes and edits. Even though that might work, Hum’s syncing wasn’t designed this way.</p>
+        <p>Hum’s folder in Dropbox meant to be read-only. If you edit any of these files by hand, or move them, you could ruin Hum’s ability to sync to it, risking your whole library. If you still want to use your audio, you’ll want to copy the files out of Dropbox and edit them elsewhere.</p>
 
         <h3>How do I enable Dropbox syncing?</h3>
-        <p>To enable Dropbox syncing, press the settings gear in the lower right corner of your library and flip the Dropbox switch. It&rsquo;ll bounce you to a screen where you can log in to your Dropbox account. A spinner will fire up and start spinning, showing you it&rsquos uploading everything. Once the spinner stops, your library is uploaded. You can also check the status of a sync in the Settings screen.</p>
+        <p>To enable Dropbox syncing, press the settings gear in the lower right corner of your library and flip the Dropbox switch. It’ll bounce you to a screen where you can log in to your Dropbox account. A spinner will fire up and start spinning, showing you it’s uploading everything. Once the spinner stops, your library is uploaded. You can also check the status of a sync in the Settings screen.</p>
 
         <h3>What happens to my library if my phone is lost or stolen?</h3>
         <p>If you sync your library to Dropbox, you can load up Hum on a new phone and sync your old library.</p>
 
         <h3>I just bought a new phone. How do I get my Hums onto my new phone?</h3>
-        <p>If you haven&rsquo;t synced your old library to Dropbox, do that first. Once you do that, you can download Hum on your new phone and sync to your existing library on Dropbox.</p>
-        <p>If you don&rsquo;t have Dropbox syncing enabled, first, plug your old phone into iTunes and back it up. Then, plug in your new phone. You&rsquo;ll want to restore your new phone from your old phone&rsquo;s backup.</p>
+        <p>If you haven’t synced your old library to Dropbox, do that first. Once you do that, you can download Hum on your new phone and sync to your existing library on Dropbox.</p>
+        <p>If you don’t have Dropbox syncing enabled, first, plug your old phone into iTunes and back it up. Then, plug in your new phone. You’ll want to restore your new phone from your old phone’s backup.</p>
 
         <h3>Can Hum import my Voice Memos?</h3>
         <p>Yep! In Voice Memos, press the share button. Hum’s icon will appear in this list. Once shared from Voice Memos, or any other app, Hum will show you a screen where you can give your new Hum a title. Press save, and you’ll find your freshly-imported Hum in your library.</p>
 
         <h3>How do I add a title to a Hum?</h3>
-        <p>The first line of the lyrics field is your title. We don&rsquo;t ever want giving your songs a title to get in the way of capturing an idea.</p>
+        <p>The first line of the lyrics field is your title. We don’t ever want giving your songs a title to get in the way of capturing an idea.</p>
 
         <h3>Will Hum add more features over time?</h3>
-        <p>Absolutely. This is just the first version of Hum. We have a laundry list of additional features that will make Hum even more useful to you, and we&rsquo;re doing our best to introduce them as quickly as possible. In fact, we have collected all our <a href="/releases/">change logs for every release</a>.</p>
+        <p>Absolutely. This is just the first version of Hum. We have a laundry list of additional features that will make Hum even more useful to you, and we’re doing our best to introduce them as quickly as possible. In fact, we have collected all our <a href="/releases/">change logs for every release</a>.</p>
 
         <h3>Can I request a feature?</h3>
         <p>Sure! We want to hear all your ideas for Hum.
@@ -119,8 +116,8 @@
           //]]>
         </script>.</p>
 
-        <h3>I&rsquo;ve found a bug, can I send it to you?</h3>
-        <p>Ugh, that&rsquo;s really lame. But, yes, please! The only way we can fix bugs is if we know about them.
+        <h3>I’ve found a bug, can I send it to you?</h3>
+        <p>Ugh, that’s really lame. But, yes, please! The only way we can fix bugs is if we know about them.
         <script type="text/javascript">
           //<![CDATA[
           <!--
