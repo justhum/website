b26fa7ea640eb3e91d8142e112f0947ac8d93fb6
Aaron Shekey <aaron@aaronshekey.com>
2017-03-30 19:46:44 -0500

    Remove stuff

--- build/blog/wp-content/themes/hum/entry-meta.php
+++
@@ -1,7 +0,0 @@
-<?php global $authordata; ?>
-<div class="entry-meta">
-	<?php the_author_image();?>
-	<span class="author">By <?php the_author(); ?></span>
-	<span class="entry-date">on <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
-	<?php edit_post_link( __( 'Edit', 'blankslate' ), "<span class=\"meta-sep\"> | </span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t" ) ?>
-</div>
