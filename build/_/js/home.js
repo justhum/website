c63923df9daa9105b47192d69789aded52704c1f
Aaron Shekey <aaron@aaronshekey.com>
2017-03-30 19:18:19 -0500

    What even is version control?

--- build/_/js/home.js
+++
@@ -1,9 +0,0 @@
-$(function(){   
-    $('.toggle-btn').click(function(){
-       $('.navigation-mobile').toggleClass("open");
-       
-       return false;
-    });
-    
-    $(".duplicate").clone().appendTo(".navigation-mobile");
-});
--- src/terms/index.php
+++ src/terms/index.php
@@ -272,7 +272,7 @@
     } ?>
 
     <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="/_/js/secondary.js"></script>
+    <script src="/_/js/main.js"></script>
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
