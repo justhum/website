10aa5df9f00a8bcd998442dcb3281ff3ffc5182c
Aaron Shekey <aaron@aaronshekey.com>
2017-11-24 17:11:59 -0600

    Header viewport

--- src/includes/header.php
+++ src/includes/header.php
@@ -12,6 +12,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
   <meta name="description" content="Hum. Songwriting for iPhone.">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> 
 
   <link rel="shortcut icon" href="/favicon.ico">
