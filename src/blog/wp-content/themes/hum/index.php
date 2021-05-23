fdd7343f4c21e4925aafdb14ef40c43e05549229
Aaron Shekey <aaron@aaronshekey.com>
2021-05-22 21:24:33 -0500

    Switch to static rendering and publish to Netlify

--- src/blog/wp-content/themes/hum/index.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-	<?php get_template_part( 'nav', 'above' ); ?>
-		<?php while ( have_posts() ) : the_post() ?>
-			<?php get_template_part( 'entry' ); ?>
-		<?php endwhile; ?>
-	<?php get_template_part( 'nav', 'below' ); ?>
-
-<?php get_footer(); ?>
